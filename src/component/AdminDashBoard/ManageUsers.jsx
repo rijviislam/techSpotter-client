f376be23c8caa4c7d3ff3ea09891290397ee0379
Rijvi Islam <rijviislam54@gmail.com>
2024-06-04 17:51:59 +0600

    feat: added make admin and make moderator button on manage user page

--- src/component/AdminDashBoard/ManageUsers.jsx
+++ src/component/AdminDashBoard/ManageUsers.jsx
@@ -18,13 +18,38 @@ export default function ManageUsers() {
   });
   if (isLoading) return <p>Loading....</p>;
   if (isError) return <p>Error....</p>;
+  const handleModerator = (id) => {
+    console.log(id);
+    axiosAllUserGet.patch(`/make-moderator/${id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        // /show a aleart
+        console.log("Moderator");
+        alert("Make Moderator successfully!");
+        // setBtnDisable(true);
+        refetch();
+      }
+    });
+  };
+  const handleAdmin = (id) => {
+    console.log(id);
+    axiosAllUserGet.patch(`/make-admin/${id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        // /show a aleart
+        console.log("Moderator");
+        alert("Make Moderator successfully!");
+        // setBtnDisable(true);
+        refetch();
+      }
+    });
+  };
   console.log(allUsers);
   return (
     <div>
       <h2 className="text-3xl">ManageUsers: {allUsers.length}</h2>
       <div className="overflow-x-auto">
         <table className="table">
-          {/* head */}
           <thead>
             <tr>
               <th>#</th>
@@ -39,8 +64,23 @@ export default function ManageUsers() {
               <tr key={user._id}>
                 <th>{idx + 1}</th>
                 <td>{user.name}</td>
-                <td>Quality Control Specialist</td>
-                <td>Blue</td>
+                <td>{user.email}</td>
+                <td>
+                  <button
+                    onClick={() => handleModerator(user._id)}
+                    className="btn btn-sm bg-cyan-400 text-gray-600"
+                  >
+                    Moderator
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => handleAdmin(user._id)}
+                    className="btn btn-sm bg-purple-700 text-white-900"
+                  >
+                    Admin
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
